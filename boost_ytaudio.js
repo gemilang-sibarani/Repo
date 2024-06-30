1bd60a9f8272649053df3a06c9c6d1ccc64b3708
Nhakchok Gemilang Sibarani <114275967+gemilang-sibarani@users.noreply.github.com>
2024-06-30 14:38:08 +0700

    Update boost_ytaudio.js

--- boost_ytaudio.js
+++ boost_ytaudio.js
@@ -1,6 +1,6 @@
 var audioCtx = new AudioContext();
 var source = audioCtx.createMediaElementSource(document.getElementsByClassName('video-stream html5-main-video')[0]);
 var gainNode = audioCtx.createGain();
-gainNode.gain.value = 6;
+gainNode.gain.value = 4;
 source.connect(gainNode);
 gainNode.connect(audioCtx.destination);
