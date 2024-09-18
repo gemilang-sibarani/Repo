daef24e06188333d1127fd385ab53ab75027c9d5
Gemilang <114275967+gemilang-sibarani@users.noreply.github.com>
2024-09-19 06:38:21 +0700

    Update boost-youtube-video-audio.js
    Penambahan penjelasan pada baris setiap kode

--- boost-youtube-video-audio.js
+++ boost-youtube-video-audio.js
@@ -1,6 +1,17 @@
+// Membuat AudioContext untuk memproses audio
 var audioCtx = new AudioContext();
+
+// Ambil elemen video dari halaman
 var source = audioCtx.createMediaElementSource(document.getElementsByClassName('video-stream html5-main-video')[0]);
+
+// Buat node pengatur volume (gain node)
 var gainNode = audioCtx.createGain();
+
+// Tingkatkan volume 4 kali lipat
 gainNode.gain.value = 4;
+
+// Hubungkan video ke gain node (pengatur volume)
 source.connect(gainNode);
+
+// Hubungkan gain node ke output (speaker)
 gainNode.connect(audioCtx.destination);
